330e5b81b7f4ea3f4c81f96b37e6a8019d41f6b6
Joel Koch <joel@joelkoch.dev>
2023-05-21 21:01:29 +0200

    Add postgres config on neon.tech

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FarmersModule } from './farmers/farmers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FarmersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ep-delicate-lake-272707-pooler.eu-central-1.aws.neon.tech',
+      port: 5432,
+      username: 'joelpaulkoch',
+      password: 'dmPTl1CZjU6w',
+      database: 'test',
+      entities: [],
+      ssl: true,
+    }),
+    FarmersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
